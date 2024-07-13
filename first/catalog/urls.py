c4c0e6501430726e1e9c1415d162d4565883665f
DeafProger <usvval@yandex.ru>
2024-07-13 19:19:45 +0700

    with added urls for catalog commit

--- first/catalog/urls.py
+++ first/catalog/urls.py
@@ -15,13 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
+from . import views
 
 app_name = 'catalog'
 
 urlpatterns = [
-    path('', include('catalog.urls')),
-    path('admin/', admin.site.urls),
+    path('contact/', views.contact),
+    path('catalog/', views.index),
+    path('', views.home),
 ]
