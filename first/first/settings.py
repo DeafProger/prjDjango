4bb23359bd62d82e75108f13a9e4db0c4f65121b
DeafProger <usvval@yandex.ru>
2024-08-13 18:27:02 +0700

    fix after remarks

--- first/first/settings.py
+++ first/first/settings.py
@@ -11,8 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-from .config import psw
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -80,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'django',
         'USER': 'postgres',
-        'PASSWORD': psw,
+        'PASSWORD': os.getenv('psw'),
     }
 }
 
